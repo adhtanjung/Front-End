3451ba568d2431570897624bda080fd960474a86
adhitanjung <adhtanjung@gmail.com>
2020-11-18 21:30:52 +0700

    added text that tells you  whether you have successfully registered or not

--- registrationForm.js
+++ registrationForm.js
@@ -5,7 +5,15 @@ function signUp() {
 	let confirmPassword = document.getElementById("confirmPassword").value;
 	if (password == confirmPassword) {
 		console.log(`Welcome ${username}, email: ${email}, password: ${password}`);
+		document.getElementById("output").style.color = "#0fe300";
+		document.getElementById("output").innerHTML = `Success!`;
+		// document.getElementById("output2").innerHTML = ``;
 	} else {
+		document.getElementById("output").style.color = "red";
+		document.getElementById(
+			"output"
+		).innerHTML = `Password has to be matched!!`;
+
 		console.log("Password has to be matched!");
 	}
 }
